2a53a9c0b5789d624e1261aa8a8ac2c7791c612f
Lampa Pampa <123573246+lampa-pampa@users.noreply.github.com>
2023-05-19 14:26:53 +0200

    Optimized main_ui.js
    added implementation of get_src()
    added preload property to audio objects
    added create_page_number()
    added refresh_arrows()

--- src/script/main_ui.js
+++ src/script/main_ui.js
@@ -20,6 +20,8 @@ class MainUI
         let music = data.GameModeToMusic[game_mode]
         this.music.main = new Audio(`src/audio/music/${music.main}`)
         this.music.bass = new Audio(`src/audio/music/${music.bass}`)
+        this.music.main.preload = true
+        this.music.bass.preload = true
         this.music.main.loop = true
         this.music.bass.loop = true
     }
@@ -36,34 +38,28 @@ class MainUI
 
     play_music()
     {
-        this.music.main.play()
-        this.music.bass.play()
+        this.music.main.oncanplay = () =>
+        {
+            this.try_to_play()
+        }
+        this.music.bass.oncanplay = () =>
+        {
+            this.try_to_play()
+        }
+        this.try_to_play()
     }
 
-    // play_music()
-    // {
-    //     this.music.main.oncanplay = () =>
-    //     {
-    //         this.try_to_play()
-    //     }
-    //     this.music.bass.oncanplay = () =>
-    //     {
-    //         this.try_to_play()
-    //     }
-    //     this.try_to_play()
-    // }
-
-    // try_to_play()
-    // {
-    //     if(game != null && game.state != GameState.end && game.properties.settings.music == true)
-    //     {
-    //         if(this.music.main.readyState == 4 && this.music.bass.readyState == 4)
-    //         {
-    //             this.music.main.play()
-    //             this.music.bass.play()
-    //         }
-    //     }
-    // }
+    try_to_play()
+    {
+        if(game != null && game.state != GameState.end && game.properties.settings.music == true)
+        {
+            if(this.music.main.readyState == 4 && this.music.bass.readyState == 4)
+            {
+                this.music.main.play()
+                this.music.bass.play()
+            }
+        }
+    }
 
     stop_music()
     {
@@ -99,21 +95,16 @@ class MainUI
         let digits = main_ui.slice_number(number)
         for(let i = 0; i < digits.length; ++i)
         {
-            html += main_ui.to_img_tag(`${data.img_dir}digits/${digits[i]}.png`, "digit")
+            html += main_ui.to_img_tag(get_src("digits", digits[i]), "digit")
         }
         return main_ui.to_div_tag(html, "value")
     }
 
-    date_to_div_tag(string)
+    str_number_to_div_tag(string)
     {
         let html = ""
         for(let i = 0; i < string.length; ++i)
-        {
-            let char = string[i]
-            if(char == "-")
-                char = "dash"
-            html += main_ui.to_img_tag(`${data.img_dir}digits/${char}.png`, "char")
-        }
+            html += main_ui.to_img_tag(get_src("digits", string[i]), "char")
         return main_ui.to_div_tag(html, "value")
     }
 
@@ -149,7 +140,11 @@ class MainUI
         if(this.is_display_hidden(display))
             this.show_display(display_type)
         display.innerHTML = windows[window_name]
-            this.turn_off_dragging_imgs(display)
+        this.turn_off_dragging_imgs(display)
+        if(isIn(window_name, ["pause", "really-quit", "really-restart"]))
+            this.display_add_opacity(display)
+        else
+            this.display_remove_opacity(display)
     }
 
     hide_display(display_type)
@@ -173,31 +168,49 @@ class MainUI
 
     refresh_menu_speed(speed)
     {
-        this.get_first_from_class("speed display").src = data.img_dir + "speed/" + 
-            data.GameSpeedToImgs[speed]
+        this.get_first_from_class("speed display").src = data.GameSpeedToImgs[speed]
     }
 
     refresh_menu_game_mode(game_mode)
     {
-        this.get_first_from_class("game-mode display").src = data.img_dir + "game_mode/" + 
-            data.GameModeToImgs[game_mode]
+        this.get_first_from_class("game-mode display").src = data.GameModeToImgs[game_mode]
+    }
+
+    create_page_number()
+    {
+        let page_number = this.get_first_from_class("page-number display")
+        page_number.innerHTML = `<img class="cur-page value"></img>`
+        page_number.innerHTML += this.str_number_to_div_tag(data.page_number_separator + data.number_of_tutorial_pages)
+        this.turn_off_dragging_imgs(page_number)
     }
 
     refresh_page(page_suffix)
     {
-        this.get_first_from_class("page display").src = data.img_dir + "how_to_play/page" + page_suffix + ".png"
-        let page_number = this.get_first_from_class("page-number display")
-        page_number.innerHTML = this.number_to_div_tag(page_suffix)
-        this.turn_off_dragging_imgs(page_number)
+        this.get_first_from_class("page display").src = get_src("how_to_play", `page${page_suffix}`)
+        this.get_first_from_class("cur-page").src = get_src("digits", page_suffix)
     }
 
-    display_add_opacity(display_type)
+    refresh_arrow(cls, disabled)
     {
-        this.get_first_from_class(`display ${display_type}`).classList.add("transparent")
+        if(disabled == true)
+            this.get_first_from_class(cls).classList.add("disabled-animation")
+        else
+            this.get_first_from_class(cls).classList.remove("disabled-animation")
     }
 
-    display_remove_opacity(display_type)
+    refresh_arrows(cls_selector, disable_left, disable_right)
     {
-        this.get_first_from_class(`display ${display_type}`).classList.remove("transparent")
+        this.refresh_arrow(`${cls_selector} left`, disable_left)
+        this.refresh_arrow(`${cls_selector} right`, disable_right)
+    }
+
+    display_add_opacity(display)
+    {
+        display.classList.add("transparent")
+    }
+
+    display_remove_opacity(display)
+    {
+        display.classList.remove("transparent")
     }
 }
